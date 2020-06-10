f8aeb082785d73bee5314dcb1859146e7750c698
Bassempro1 <bassempro1@gmail.com>
2020-06-10 08:07:19 -0600

    Add styling to Home and Full Stack pages

--- DataHappy/Models/Conference.cs
+++ DataHappy/Models/Conference.cs
@@ -9,11 +9,21 @@ namespace DataHappy.Models
     public class Conference
     {
         public int ID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
         public string Description { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
         public string City { get; set; }
 
     }
