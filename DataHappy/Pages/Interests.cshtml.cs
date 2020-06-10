160e437c36af8e5bfe7bf7712187225bb0d79fbb
Bassempro1 <bassempro1@gmail.com>
2020-06-10 01:50:13 -0600

    Add Interest razor page

---
+++ DataHappy/Pages/Interests.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DataHappy.Pages
+{
+    public class InterestsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
